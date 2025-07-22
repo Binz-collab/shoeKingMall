ccfe760fc17b23a213a75300e5d270707e0cdd50
Binz-collab <singriet0824@hs.ac.kr>
2025-07-22 19:25:57 +0900

    fix:  화면 출력 비율 수정

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,23 @@ function App() {
 
   return (
     <div className="app-frame">
-      <nav className="menu-fixed" style={{position: 'relative', width: '100%', height: 69, display: 'flex', alignItems: 'center', justifyContent: 'flex-end', padding: '0 20px', boxSizing: 'border-box'}}>
+      <nav className="menu-fixed">
         {/* 메뉴바 배경 SVG */}
-        <svg width="430" height="69" viewBox="0 0 430 69" fill="none" xmlns="http://www.w3.org/2000/svg" style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0}}>
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 430 69"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 0
+          }}
+        >
           <rect width="430" height="69" fill="black"/>
         </svg>
         {/* 장바구니+뱃지 */}
